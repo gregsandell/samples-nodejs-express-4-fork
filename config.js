ccd0b36019d13f0d869277e2f4c4a8558dd7c51a
Greg Sandell <gsandell_99@yahoo.com>
2023-10-18 11:08:20 -0700

    Change environment settings file from testenv to .env, add and use env-cmd

--- config.js
+++ config.js
@@ -3,14 +3,13 @@ const dotenv = require('dotenv');
 const fs = require('fs');
 
 // Read environment variables from "testenv". Override environment vars if they are already set. https://www.npmjs.com/package/dotenv
-const TESTENV = path.resolve(__dirname, 'testenv');
+const TESTENV = path.resolve(__dirname, '.env');
 if (fs.existsSync(TESTENV)) {
   const envConfig = dotenv.parse(fs.readFileSync(TESTENV));
   Object.keys(envConfig).forEach((k) => {
     process.env[k] = envConfig[k];
   });
 }
-
 var ISSUER = process.env.ISSUER || 'https://{yourOktaDomain}.com/oauth2/default';
 var CLIENT_ID = process.env.CLIENT_ID || '{clientId}';
 var CLIENT_SECRET = process.env.CLIENT_SECRET || '{clientSecret}';
